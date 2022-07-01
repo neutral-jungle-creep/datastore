13e8b1ddb1b286479f7394635c2ba57c3d86591c
Arseny <arrrseny11@gmail.com>
2022-07-01 22:54:03 +0300

    add change_lines.py

---
+++ change_lines.py
@@ -0,0 +1,15 @@
+from loguru import logger
+
+
+def change(word: str) -> list:
+    '''Вернет список строк, в которых подстрока {word} изменена на полученную из консоли'''
+    data = []
+    new_word = input('Слово на которое заменить: ')
+    with open('search_word_presets.csv', 'r', encoding='utf-8') as search_word_presets:
+        head = search_word_presets.readline()  # первая строка
+        for line in search_word_presets.readlines():
+            new_line = line.replace(word, new_word)
+            data.append(new_line)
+            logger.info(f'Новая строка - {new_line}')
+    logger.debug(f'Отредактировано {len(data)} строк')
+    return data
