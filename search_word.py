e1a8aa41418d4bd4a118302af527e6eb83504230
Arseny <arrrseny11@gmail.com>
2022-07-01 23:00:01 +0300

    fix search_word.py

--- search_word.py
+++ search_word.py
@@ -1,7 +1,7 @@
 '''
 DOCSTRING: Ищет записи с введенным словом в человеческом запросе.
 Принимает название файла без расширения и слово для поиска.
-Возвращает файл found_words_{file_name}.csv, содержащий только выбранные строки.
+Возвращает файл search_word_{file_name}.csv, содержащий только выбранные строки.
 '''
 import pandas as pd
 import csv
@@ -23,7 +23,7 @@ def query_check(item: str) -> bool:
 
 
 def search(file: str) -> None:
-    '''Запишет в файл с именем 'result_{file_name}.csv строки с введенным словом.'''
+    '''Запишет в файл с именем 'search_word_{file_name}.csv строки с введенным словом.'''
     df = pd.read_csv(f'{file}.csv', delimiter='|')
     df['word'] = 0
 
@@ -32,9 +32,13 @@ def search(file: str) -> None:
     trunc_df = df.loc[df['word'] == True]
     trunc_df = trunc_df.drop('word', axis=1)
     trunc_df['PresetID'] = trunc_df['PresetID'].astype(int)
-    trunc_df.to_csv(f'found_words_{file}.csv', sep='|', quoting=csv.QUOTE_NONE, index=False)
+    trunc_df.to_csv(f'search_word_{file}.csv', sep='|', quoting=csv.QUOTE_NONE, index=False)
+    logger.debug(f'Найдено {len(trunc_df)} строк со словом {word_for_search}')
 
 
 file_name = input('file name: ')  # название файла (presets)
 word_for_search = input('word: ')  # слово, которое будет искать скрипт в файле в колонке Query
-# search(file_name)
+
+
+if __name__ == '__main__':
+    search(file_name)
