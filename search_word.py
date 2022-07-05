1bcefc58fbd88f4316f126de4f7f87627cf7b3b7
neutral-jungle-creep <arrrseny11@gmail.com>
2022-07-05 09:54:32 +0300

    rewrite search_word.py

--- search_word.py
+++ search_word.py
@@ -10,29 +10,23 @@ from loguru import logger
 
 def main(file: str) -> None:
     '''Запишет в файл с именем 'search_word_{file_name}.csv строки с введенным словом.'''
-    df = pd.read_csv(f'C:\\Pепозиторий\\datastore-1\\indices\\{file}.csv', delimiter='|')
-    df['word'] = 0
+    with open(f'C:\\Pепозиторий\\datastore-1\\indices\\{file}.csv', 'r', encoding='utf-8') as file_input:
+        head = file_input.readline()
+        for line in file_input.readlines():
+            query = line[1:line.index(')')].split()
+            if word_for_search in query:
+                new_data.append(line)
+                logger.info(f'запрос, прошедший проверку: {query}')
 
-    df['word'] = df['Search Query'].apply(query_check)
-
-    trunc_df = df.loc[df['word'] == True]
-    trunc_df = trunc_df.drop('word', axis=1)
-    trunc_df['PresetID'] = trunc_df['PresetID'].astype(int)
-    trunc_df.to_csv(f'search_word_{file}.csv', sep='|', quoting=csv.QUOTE_NONE, index=False)
-    logger.debug(f'В файл search_word_{file}.csv записано {len(trunc_df)} строк со словом {word_for_search}')
-
-
-def query_check(item: str) -> bool:
-    '''Вернет правду, если в проверяемом списке есть введенное слово.'''
-    item = item.replace('(', '').replace(')', '').split()
-    result = any(map(lambda i: i == word_for_search, item))
-    if result:
-        logger.info(f'запрос, прошедший проверку: {item}')
-    return result
+    with open('search_word_{file_name}.csv', 'w', encoding='utf-8') as file_output:
+        file_output.write(head)
+        file_output.writelines(new_data)
+        logger.debug(f'В файл search_word_{file}.csv записано {len(new_data)} строк со словом {word_for_search}')
 
 
 word_for_search = input('Слово: ')  # слово, которое будет искать скрипт в файле в колонке Query
 file_name = input('Название файла: ')  # название файла (presets)
+new_data = []
 
 
 if __name__ == '__main__':
