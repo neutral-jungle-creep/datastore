d12f926968c77b15cb4f58aac0f3bd3a3bfafbb4
neutral-jungle-creep <arrrseny11@gmail.com>
2022-07-05 22:48:26 +0300

    add docs v2_search.py

--- v2_search.py
+++ v2_search.py
@@ -1,31 +1,36 @@
+'''
+DOCSTRING: Создает отчет о выдаче человеческого запроса с введенным словом и при его изменении.
+Принимает строку с изменяемым словом и со словом на которое требуется заменить.
+'''
 from loguru import logger
 import requests
 import json
 
 
 def main() -> None:
-    old_word = input('Заменяемое слово: ')
+    '''Запишет в файлы результаты выдачи до и после изменения слова в человеческом запросе.'''
+    new_word = input('Новое слово: ')
     with open(f'search_word_{file_name}.csv', 'r', encoding='utf-8') as file_input:
         file_input.readline()
         for line in file_input.readlines():
-            request_old = line[1:line.index(')')]
-            request_new = line[1:line.index(')')].replace(old_word, new_word)
-            old_data.append(request_v2(request_old) + '\n')
-            new_data.append(request_v2(request_new) + '\n')
+            query = line[1:line.index(')')]
+            if old_word in query.split():
+                request_old = query
+                request_new = query.replace(old_word, new_word)
+                data.append(f'{request_v2(request_old)}|{request_v2(request_new)}\n')
         write()
 
 
 def write() -> None:
-    with open(f'v2_search_old_{file_name}.csv',
-              'w', encoding='utf-8') as old_data_output, open(f'v2_search_new_{file_name}.csv',
-                                                              'w', encoding='utf-8') as new_data_output:
-        old_data_output.write(new_head)
-        new_data_output.write(new_head)
-        old_data_output.writelines(old_data)
-        new_data_output.writelines(new_data)
+    '''Запишет сформированный отчет в файл.'''
+    with open(f'v2_search_change_{file_name}.csv', 'w', encoding='utf-8') as file_output:
+        file_output.write(new_head)
+        file_output.writelines(data)
+        logger.debug(f'Проверено {len(data)} строк со словом {old_word}')
 
 
 def request_v2(item: str) -> str:
+    '''Вернет строку с отчетом по записи.'''
     logger.info(f'Проверка человеческого запроса: ({item})')
 
     link = 'http://exactmatch-common.wbx-search-internal.svc.k8s.dataline/v2/search?'
@@ -36,16 +41,15 @@ def request_v2(item: str) -> str:
         response = session.get(link, params=query)
         result = json.loads(response.text)
         logger.debug(f'Результат: {result}')
-    return '|'.join([value for value in result.values()])
+    return f'{result["name"]}|{result["query"]}|{result["shardKey"]}'
 
 
-old_data = []
-new_data = []
-new_head = 'name|query|shardKey|filters\n'
+data = []
+new_head = 'name|query|shardKey|newname|newquery|newshardKey\n'
 
 
 if __name__ == '__main__':
     logger.add('logs.log')
-    new_word = input('Новое слово: ')
+    old_word = input('Заменяемое слово: ')
     file_name = input('Название файла: ')
     main()
