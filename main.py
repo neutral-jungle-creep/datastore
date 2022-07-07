99ac06b5622d70a30170a66f9542c0d1cba3188e
neutral-jungle-creep <arrrseny11@gmail.com>
2022-07-07 15:24:09 +0300

    add func report to main.py

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 '''
 DOCSTRING:
-
+1. сандали - искать по ручке запрос с двумя И вместо одной
+1.1 если онлайн поиск, править запись
+1.2 иначе удалить запись
 '''
-import search_word
-import change_lines
-import del_lines
+import functions
 from loguru import logger
 
 
 def mine() -> None:
-    search_word.main(file)
-    change_data = change_lines.main(word, file)
-    logger.debug(f'Получено {len(change_data)} строк')
+    data = functions.read(file_name)
+    head = functions.head
+    for line in data:
+        if functions.search_word(old_word, line):
+            analiz(line)
+    functions.head = 'name|query|shardKey|new name|new query|new shardKey\n'
+    functions.write('C:\\CodePy\\wb\\datastore\\main_reports\\additional_search', additional_search)
+    functions.write('C:\\CodePy\\wb\\datastore\\main_reports\\merger', merger)
+    functions.write('C:\\CodePy\\wb\\datastore\\main_reports\\delete', delete)
 
-    clean_data = del_lines.main(word, file)
-    logger.debug(f'Получено {len(clean_data)} строк')
 
-    write(clean_data, change_data)
+def analiz(line: str):
+    query = line[1:line.index(')')]
+    request_old = functions.request_v2(query)
+    request_new = functions.request_v2(query.replace(old_word, new_word))
+    logger.info(f'old query: {request_old} | new query: {request_new}\n')
+    if '&_t0=' in request_new["query"]:
+        report(additional_search, request_old, request_new)
+        return 1
+    elif '&_t1=' in request_new["query"]:
+        report(merger, request_old, request_new)
+        return 2
+    else:
+        report(delete, request_old, request_new)
+        return 3
 
 
-def write(clean_data: list, change_data: list) -> None:
-    with open(f'main_{file}.csv', 'w', encoding='utf-8') as data:
-        data.writelines(clean_data)
-        data.writelines(change_data)
-        logger.debug(f'В файл main_{file}.csv записано {len(clean_data) + len(change_data)} строк')
+def report(word: list, old: dict, new: dict) -> None:
+    '''Примет название коллекции и два словаря, добавит в коллекцию отформатированный отчет'''
+    word.append(f'{functions.format_report(old)}|{functions.format_report(new)}\n')
 
 
 if __name__ == '__main__':
     logger.add('logs.log')
-    word = search_word.word_for_search
-    file = search_word.file_name
+    file_name = input('Название файла: ')  # название файла
+    old_word = input('Заменяемое слово: ')  # слово, которое будет искать скрипт в файле в колонке Query
+    new_word = input('Новое слово: ')  # слово, на которое скрипт заменит найденное старое
+    new_data = []
+    additional_search, merger, delete = [], [], []
     mine()
